e7c0b32b1e84f0ea89644b9e5552984898e32a90
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-05-14 17:04:03 -0400

    update js with desired functionality

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 
 
+
 document.addEventListener("DOMContentLoaded", function(event) {
     // console.log("DOM fully loaded and parsed");
-  var status =   document.querySelector(".status");
-  status.innerHTML = 'this worked'
-  });
+  var status = document.querySelector(".status");
+  status.innerHTML = 'this worked';
+});
 
-
-document.addEventListener("click", function(event){
-
-    var status = document.querySelector(".switch");
-    status.innerHTML = 'clicked'
-  });
+var lightSwitch = document.querySelector(".switch")
+lightSwitch.addEventListener("click", function(event){
+  lightSwitch.classList.toggle('on');
+  var status = document.querySelector(".status");
+  var body = document.querySelector("body");
+  if (lightSwitch.classList.contains('on')) {
+      body.classList.remove("dark")
+      body.classList.add("light")
+      status.innerHTML = "Hey,it's bright in here!"
+  } else {
+      body.classList.remove('light');
+      body.classList.add("dark")
+      status.innerHTML = "Hey,lights are off!"
+  }
+});
