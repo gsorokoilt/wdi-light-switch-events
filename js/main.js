cfe5f300bfbb121b025c81168587ef860ebcbd29
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-05-14 15:47:22 -0400

    add event listener

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 
 
+document.addEventListener("DOMContentLoaded", function(event) {
+    // console.log("DOM fully loaded and parsed");
+  var status =   document.querySelector(".status");
+  status.innerHTML = 'this worked'
+  });
+
+
+document.addEventListener("click", function(event){
+
+    var status = document.querySelector(".switch");
+    status.innerHTML = 'clicked'
+  });
